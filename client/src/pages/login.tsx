b724c9c456b04fb9d26d0f765f8877788523a3ac
Linh <mlinhng05@gmail.com>
2021-07-22 08:34:05 +0200

    added SASS

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,46 @@
+// this page has an animated background and a login card with link to signup / recover password
+
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="page-wrapper">
+                
+                <div className="login card">
+                    <h1>Login</h1>
+
+                    <form className="login form" action="" method="POST">
+                        <div className="login fields col">
+                            <div className="login field col">
+                                <label htmlFor="email">Email</label>
+                                <input type="email" name="email" id="email"/>
+                            </div>
+                            
+                            <div className="login field col">
+                                <label htmlFor="username">Username</label>
+                                <input type="text" name="username" id="username"/>
+                            </div>
+                            
+                            <div className="login field col">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" name="password" id="password"/>
+                            </div>
+                        </div>
+
+                        <button type="button">
+                            Login
+                        </button>                        
+                    </form>
+
+                    <Link to="/account/recover">Forgot your password?</Link>
+                    <p>Haven't got an account? <span><Link to="/register">Sign up</Link></span></p>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
